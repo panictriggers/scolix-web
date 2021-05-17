f076fb82c3ba6626bb178037500bd644dc11a051
J0eppp <joep.van.dijk04@gmail.com>
2021-05-17 15:24:29 +0200

    Uses the flag module to determine wto choose to run this as dev or not

--- cmd/api0/main.go
+++ cmd/api0/main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 	"scolix/cmd/api0/httpHandlers"
 	"scolix/pkg/httpmiddleware"
 )
 
 func main() {
+	var dev = flag.Bool("dev", false, "enable dev debug messages")
+	flag.Parse()
 	router := mux.NewRouter().StrictSlash(false)
 
 	// Use the middlewares
 	router.Use(httpmiddleware.EnableCors)
 	router.Use(httpmiddleware.SetResponseTypeJSON)
-	router.Use(httpmiddleware.RequestLogger)
+
+	// Dev middlewares
+	if *dev == true {
+		router.Use(httpmiddleware.RequestLogger)
+	}
 
 
 	router.HandleFunc("/", httpHandlers.Index)
 
-	if len(os.Args) > 1 {
-		if os.Args[1] == "-dev" {
-			log.Println("The server is running...")
-		}
+	if *dev == true {
+		log.Println("The server is running")
 	}
 
 	log.Fatal(http.ListenAndServe(":8080", router))
---
+++ go.mod
@@ -0,0 +1,8 @@
+module scolix
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/urfave/negroni v1.0.0
+)
